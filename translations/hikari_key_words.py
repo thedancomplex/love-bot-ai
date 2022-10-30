409d2c1b43c37fa56fc3e1a3bcece61697f73b2c
Daniel M. Lofaro <dan@danlofaro.com>
2022-10-29 21:52:02 -0400

    things are using a { instead of a [ most the time, maybe i shoudl switch to [ for everything

--- translations/hikari_key_words.py
+++ translations/hikari_key_words.py
@@ -50,7 +50,7 @@ class Hikari_Keywords:
     '高い気'
   }
 
-  telling_storties_talking_about_stuff={
+  telling_stories_talking_about_stuff={
     '話して',
     '話',
     'ひまー',
@@ -794,32 +794,33 @@ class Hikari_Keywords:
   }
 
 
-  all_topics={
-self.recommended_openers,
-self.asking_for_advise_from_hikari,
-self.telling_stories_talking_about_stuff,
-self.games,
-self.generic_greetings,
-self.expressions_of_agreement,
-self.wondering_and_questions,
-self.words_of_thanks,
-getting_hikaris_attention,
-complimenting_hikari,
-caring_for_caring_about_hikari,
-expressing_affection,
-fun_times,
-sad_times,
-tough_times,
-anxiety_worry,
-physical_condition,
-doing_ones_best,
-impressed_suprised_embarrassed,
-dreams_and_aspirations,
-names_for_hikari,
-asking_about_hikari,
-about_hikaris_family,
-hikaris_moods_how_is_she,
-joking_around_with_hikari,
-familiar_phrases_etc,
-general_communication,
-
+  all_topics=[
+    recommended_openers,
+    asking_for_advise_from_hikari,
+    telling_stories_talking_about_stuff,
+    games,
+    generic_greetings,
+    expressions_of_agreement,
+    wondering_and_questions,
+    words_of_thanks,
+    getting_hikaris_attention,
+    complimenting_hikari,
+    caring_for_caring_about_hikari,
+    expressing_affection,
+    fun_times,
+    sad_times,
+    tough_times,
+    anxiety_worry,
+    physical_condition,
+    doing_ones_best,
+    impressed_suprised_embarrassed,
+    dreams_and_aspirations,
+    names_for_hikari,
+    asking_about_hikari,
+    about_hikaris_family,
+    hikaris_moods_how_is_she,
+    joking_around_with_hikari,
+    familiar_phrases_etc,
+    general_communication,
+    no_topic
+  ]
--- translations/love_bot_ai.py
+++ translations/love_bot_ai.py
@@ -2,32 +2,4 @@ import hikari_key_words as hkw
 
 hkw = hkw.Hikari_Keywords()
 
-hikari_all={
-hkw.recommended_openers,
-asking_for_advise_from_hikari,
-telling_stories_talking_about_stuff,
-games,
-generic_greetings,
-expressions_of_agreement,
-wondering_and_questions,
-words_of_thanks,
-getting_hikaris_attention,
-complimenting_hikari,
-caring_for_caring_about_hikari,
-expressing_affection,
-fun_times,
-sad_times,
-tough_times,
-anxiety_worry,
-physical_condition,
-doing_ones_best,
-impressed_suprised_embarrassed,
-dreams_and_aspirations,
-names_for_hikari,
-asking_about_hikari,
-about_hikaris_family,
-hikaris_moods_how_is_she,
-joking_around_with_hikari,
-familiar_phrases_etc,
-general_communication,
-
+print(hkw.all_topics)
